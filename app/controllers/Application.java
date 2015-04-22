1dedb5c06e116e1a9ab2738752da8e1d70c80e94
Yerganat Merzentay <yerganat.merzentay@bas.kz>
2015-04-22 20:42:09 +0600

    sql

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,26 @@
 package controllers;
 
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
 
+import static play.data.Form.form;
+
 public class Application extends Controller {
+
+    public static class Login {
+        public String email;
+        public String password;
+
+        public String validate() {
+            if (email ==null || email.isEmpty()) {
+                return "Invalid user or password";
+            }
+            return null;
+        }
+    }
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
@@ -16,8 +31,23 @@ public class Application extends Controller {
     }
 
     public static Result login() {
-        return ok(login.render("Your new application is ready."));
+        return ok(login.render(form(Login.class)));
     }
+
+    public static Result authenticate() {
+        Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+        //if (loginForm.hasErrors()) {
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+
+        } else {
+            session().clear();
+            session("email", loginForm.get().email);
+            return redirect(routes.Application.index());
+        }
+    }
+
+
 
     public static Result checkout() {
         return ok(checkout.render("Your new application is ready."));
@@ -26,4 +56,8 @@ public class Application extends Controller {
         return ok(shop.render("Your new application is ready."));
     }
 
+
+
 }
+
+
